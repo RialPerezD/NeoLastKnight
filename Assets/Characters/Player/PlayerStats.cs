025fc726ccb4cfb760e2622d600c1decb6dfec10
Vicente <bellotvi@esat-alumni.com>
2025-04-27 14:32:55 +0200

    implemented the shaders for low damage and damage taken

--- Assets/Characters/Player/PlayerStats.cs
+++ Assets/Characters/Player/PlayerStats.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public class PlayerStats : MonoBehaviour, Combat
 {
@@ -11,9 +14,59 @@ public class PlayerStats : MonoBehaviour, Combat
     public float coins = 0;
     public int damage = 1;
 
+    /* Damage Effect */
+    Renderer renderer;
+    Material material;
+    float tick;
+    Material splashDamage;
+    Canvas canvasplayer;
+    bool hitted = false;
+    bool Splash = false;
+    public float maxSplashEffect = 2;
+
+
+
     void Start()
     {
         ui = GameObject.Find("UI").GetComponent<BaseUi>();
+        renderer = GetComponent<Renderer>();
+        material = renderer.material;
+        canvasplayer = GameObject.Find("UI").GetComponent<Canvas>();
+        UnityEngine.Canvas[] childCanvases = canvasplayer.GetComponentsInChildren<Canvas>(true);
+        foreach (Canvas canvas in childCanvases)
+        {
+            if (canvas.name == "SplashDamage")
+            {
+                UnityEngine.UI.Image rawImage = canvas.GetComponent<UnityEngine.UI.Image>();
+                splashDamage = rawImage.material;
+                break;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(hp < 0.2f * maxHp)
+        {
+            tick += (1.2f) * Time.deltaTime;
+            float flash = Mathf.Abs(Mathf.Sin(tick) * 0.8f);
+            material.SetFloat("_Flash", flash);
+        }
+        if (hitted && !Splash)
+        {
+            splashDamage.SetFloat("_darkening", maxSplashEffect);
+            Splash = true;
+        }
+        if (Splash)
+        {
+            float take = (splashDamage.GetFloat("_darkening") - 0.2f * 20.0f * Time.deltaTime);
+            splashDamage.SetFloat("_darkening", take);
+            if (take <= 0.0f)
+            {
+                Splash = false;
+                hitted = false;
+            } 
+        }
     }
 
     public void Interactua(int objectType)
@@ -35,7 +88,7 @@ public class PlayerStats : MonoBehaviour, Combat
     public bool RecibeDamage(int ammount)
     {
         hp -= ammount;
-
+        hitted = true;
         ui.CambiaVida((hp / maxHp) * 100f);
 
         if (hp <= 0)
