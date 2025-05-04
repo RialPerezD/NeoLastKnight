0792cf80090d53900df18c88d306460b6b8a31f7
Vicente <bellotvi@esat-alumni.com>
2025-05-04 21:10:20 +0200

    added phone input

--- Assets/Characters/Player/PlayerMovement.cs
+++ Assets/Characters/Player/PlayerMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine.Tilemaps;
 public class PlayerMovement : MonoBehaviour, UpdatePosition
 {
     public Tilemap obstacle_tilemap; // Tilemap de los obstáculos
-
     public Vector2Int posicion;
 
     Vector2Int lastInputDirection;
@@ -17,6 +16,13 @@ public class PlayerMovement : MonoBehaviour, UpdatePosition
     int alturaMovCam = 3;
     int limiteInferior = -1;
     bool camaraForzada = false;
+
+    // Swipe variables
+    Vector3 touch_start_pos;
+    Vector3 touch_current_pos;
+    Vector3 touch_delta;
+    bool is_touching = false;
+    float lapse = 0.0f;
 
     private void Awake()
     {
@@ -63,6 +69,9 @@ public class PlayerMovement : MonoBehaviour, UpdatePosition
 
     Vector2Int GetInputDirection()
     {
+#if UNITY_ANDROID
+            return GetSwipeInput();
+#else
         if (Input.GetKeyDown(KeyCode.W))
             return new Vector2Int(0, 1);     // Arriba
         if (Input.GetKeyDown(KeyCode.S))
@@ -73,6 +82,68 @@ public class PlayerMovement : MonoBehaviour, UpdatePosition
             return new Vector2Int(1, 0);     // Derecha
 
         return Vector2Int.zero; // No se presionó ninguna tecla
+    }
+
+    Vector2Int GetSwipeInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touch_start_pos = Camera.main.ScreenToWorldPoint(touch.position);
+                touch_start_pos.z = 0;
+                is_touching = true;
+                lapse = 0f;
+            }
+
+            if (is_touching)
+            {
+                lapse += Time.deltaTime;
+            }
+
+            if (touch.phase == TouchPhase.Moved && is_touching)
+            {
+                touch_current_pos = Camera.main.ScreenToWorldPoint(touch.position);
+                touch_current_pos.z = 0;
+                touch_delta = touch_current_pos - touch_start_pos;
+            }
+
+            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                is_touching = false;
+                lapse = 1.0f;
+            }
+
+            if (!is_touching && lapse >= 1.0f)
+            {
+                lapse = 0.0f;
+                Vector3 direction = touch_delta.normalized;
+                touch_delta = Vector3.zero;
+
+                Vector3 swipeDir = GetSwipeDirection(direction);
+
+                if (swipeDir == Vector3.up) return Vector2Int.up;
+                if (swipeDir == Vector3.down) return Vector2Int.down;
+                if (swipeDir == Vector3.left) return Vector2Int.left;
+                if (swipeDir == Vector3.right) return Vector2Int.right;
+            }
+        }
+
+        return Vector2Int.zero;
+    }
+
+    Vector3 GetSwipeDirection(Vector3 dir)
+    {
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            return dir.x > 0 ? Vector3.right : Vector3.left;
+        }
+        else
+        {
+            return dir.y > 0 ? Vector3.up : Vector3.down;
+        }
     }
 
     public void UpdatePosicion(Vector2Int pos)
